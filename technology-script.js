33e04a4183ef21dee02888166953ee52f50e1217
Ümmühan Keleş <ummuhankeles4@gmail.com>
2022-08-29 16:59:06 +0300

    Add the technology category

---
+++ technology-script.js
@@ -0,0 +1,32 @@
+const technology = document.querySelector('#technology');
+const datas = document.querySelector('.technology__container-row');
+
+window.addEventListener('load', getTechnology);
+async function getTechnology() {
+    let technologyData = await fetch('https://newsapi.org/v2/top-headlines?country=tr&category=technology&apiKey=341bbc68b3b843159af93e0fae7dda1f').then(res => res.json());  
+    console.log(technologyData);
+    let item = '';
+    technologyData.articles.forEach(e => {
+        item += `
+        <div class="col-md-6 mb-4">
+            <img src="${e.urlToImage}" alt="">
+            <div class="description">
+                <div class="desc-bottom">
+                    <h5>
+                        <a href="#">${e.title}</a>
+                    </h5>
+                    <div class="row-right-bottom">
+                        <span>
+                            <a href="#">${e.author}</a>
+                        </span>
+                        <span>
+                            ${e.publishedAt.slice(0,10)} <span>•</span> 3 min read <span><i class="fa-solid fa-star"></i></span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        datas.innerHTML = item;
+    });
+}
